e41847fa8032a93fd354543e3ef0c8a788e85da1
David Ko <davidko@cs.utexas.edu>
2015-07-24 02:53:01 -0500

    still checking error cases

--- assignment1/davidko-asg1.py
+++ assignment1/davidko-asg1.py
@@ -22,29 +22,74 @@ def main():
 
         print()
 
+    counter = num_cols * num_rows
 
-    x_cord = int(input("yoo whats the x-cord you want it to be at:"))
-    y_cord = int(input("cmon bro you gotta tell me the y-cord too"))
+    while(counter > 0):
 
-    while(x_cord > num_rows -1): 
-        x_cord = int(input("yooo you got the wrong x cord, choose a new one: "))
-     
-    while(y_cord > num_cols -1):
-        y_cord = int(input("yooo you got the wrong y cord, choose a new one: "))
-    
+        x_cord = int(input("yoo whats the x-cord you want it to be at: "))
+        y_cord = int(input("cmon bro you gotta tell me the y-cord too: "))
 
-    matrix[x_cord][y_cord] = "X"
-    matrix[x_cord-1][y_cord] = "X"
-    matrix[x_cord+1][y_cord] = "X"
-    matrix[x_cord][y_cord-1] = "X"
-    matrix[x_cord][y_cord+1] = "X"
+        #reprompt
+        while(x_cord > num_rows -1): 
+            x_cord = int(input("yooo you got the wrong x cord, choose a new one: "))
+         
+        while(y_cord > num_cols -1):
+            y_cord = int(input("yooo you got the wrong y cord, choose a new one: "))
+        
+        #base case
+        matrix[y_cord][x_cord] = "X"
 
-    print("nice bro, now ur matrix looks like this: ")
+        counter -= 1
 
-    for i in range(len(matrix)):
-        for j in range(num_cols):
-            print(matrix[i][j], end="")
+        if(x_cord == num_rows-1):
+            if(matrix[y_cord-1][x_cord] != "X"):
+                counter -= 1
+            matrix[y_cord-1][x_cord] = "X"
+        elif(x_cord == 0):
+            if(matrix[y_cord+1][x_cord] != "X"):
+                counter -= 1
+            matrix[y_cord+1][x_cord] = "X"
+        else:
+            matrix[y_cord-1][x_cord] = "X"
+            matrix[y_cord+1][x_cord] = "X"
+            counter -= 2
 
-        print()
+        if(y_cord == num_cols-1):
+            if(matrix[y_cord][x_cord-1] != "X"):
+                counter -= 1
+            matrix[y_cord][x_cord-1] = "X"
+        elif(y_cord == 0):
+            if(matrix[y_cord][x_cord+1] != "X"):
+                counter -= 1
+            matrix[y_cord][x_cord+1] = "X"
+        else:
+            matrix[y_cord][x_cord-1] = "X"
+            matrix[y_cord][x_cord+1] = "X"
+            counter -= 2
+
+        print("nice bro, now ur matrix looks like this: ")
+
+        for i in range(len(matrix)):
+            for j in range(num_cols):
+                print(matrix[i][j], end="")
+            print()
+
+
+    print("Broooo you ran out of spaces to fill. we done here!")
+
+    #matrix[x_cord][y_cord] = "X"
+    #matrix[x_cord-1][y_cord] = "X"
+    #matrix[x_cord+1][y_cord] = "X"
+    #matrix[x_cord][y_cord-1] = "X"
+    #matrix[x_cord][y_cord+1] = "X"
+
+#    print("nice bro, now ur matrix looks like this: ")
+#
+#    for i in range(len(matrix)):
+#        for j in range(num_cols):
+#            print(matrix[i][j], end="")
+#
+#        print()
+
 
 main()
